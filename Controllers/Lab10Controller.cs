da350cd0f6ca3234deba104246a5215cd1238c31
ValentynaFedorchuk <154245348+ValentynaFedorchuk@users.noreply.github.com>
2024-11-15 21:53:38 +0200

    Add files via upload

---
+++ Controllers/Lab10Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class Lab10Controller : Controller
+    {
+        // GET: Lab10
+        public ActionResult ListOfDoctors()
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            using (var db = new HospitalEntities()) 
+            { 
+                doctors = db.Doctors.OrderByDescending(x => x.Age)
+                    .ThenBy(x => x.FullName).ToList();
+            }
+            return View(doctors);
+        }
+
+        [HttpGet]
+        public ActionResult DoctorDetails(Guid doctorId) 
+        {
+            Doctor model = new Doctor();
+            using (var db = new HospitalEntities()) 
+            {
+                model = db.Doctors.Find(doctorId);
+            }
+            return View(model);
+        }
+    }
+}
